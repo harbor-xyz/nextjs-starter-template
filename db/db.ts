e5ddc54a55537f6c78dd2e35d11d0a9dda81f849
Avinash Bhardwaj <avinashb97@gmail.com>
2025-03-05 01:26:21 +0530

    update db setup

--- db/db.ts
+++ db/db.ts
@@ -1,6 +1,4 @@
 import { config } from 'dotenv';
 import { drizzle } from 'drizzle-orm/postgres-js';
-import postgres from 'postgres';
-config({ path: '.env' }); // or .env.local
-const client = postgres(process.env.DATABASE_URL!);
-export const db = drizzle({ client });
+config({ path: '.env' });
+export const db = drizzle(process.env.DATABASE_URL!);
