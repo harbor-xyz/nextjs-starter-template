875669575f7a380d3f6abdedbeea081e015f7948
Prakash Sanker <praks5432@gmail.com>
2025-03-06 16:59:30 +0530

    more logs

--- lib/vercel-sdk.ts
+++ lib/vercel-sdk.ts
@@ -18,7 +18,8 @@ dotenv.config();
 
 console.log("PROCESS ENV AFTER CONFIG");
 console.log(process.env);
-
+console.log(process.env.OPENAI_API_KEY);
+console.log(process.env.ANTHROPIC_API_KEY);
 const openAiKey = process.env.OPENAI_API_KEY;
 const anthropicKey = process.env.ANTHROPIC_API_KEY;
 
